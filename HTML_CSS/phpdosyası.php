5f3080af4854056f2db0a63c537391b81842083c
Sude <167243165+Sood3@users.noreply.github.com>
2024-05-14 23:40:44 +0300

    Gün 9
    İletişim sayfasının javascript ve vue kodları bitirildi php dosyası ile girilen verileri alıp başka sayfada gösterme işlemi yapıldı

---
+++ HTML_CSS/phpdosyası.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Teknolojileri Proje Ödevi PHP sayfası</title>
+    <link href="style/PHPstyle.css" rel="stylesheet" />
+</head>
+<body>
+
+<div class="container">
+        <div class="box">
+            <h2><?php $name = $_POST['name']; echo "Form başarıyla gönderildi. Teşekkürler, $name!"."<br>" ?></h2>
+            <p><?php
+
+                $name = $_POST['name'];
+                $mail = $_POST['mail'];
+                $shortcontent = $_POST['short-content'];
+                $explanation = $_POST['explanation'];
+                $school = $_POST['school'];
+
+                echo "Ad: " . htmlspecialchars($name) . "<br>";
+                echo "Email: " . htmlspecialchars($mail) . "<br>";
+                echo "Okul: " . htmlspecialchars($school) . "<br>";
+                echo "Konu: " . htmlspecialchars($shortcontent) . "<br>";
+                echo "Açıklama: " . htmlspecialchars($explanation) . "<br>";
+
+            ?></p>
+            <div class="button-container">
+                <button onclick="Return()" type="button">Ana Sayfaya dön</button>
+            </div>
+        </div>
+        
+    </div>
+    
+    <script>
+        function Return(){
+            window.location.href="AnaSayfa.html"
+        }
+
+    </script>
+</body>
+</html>
