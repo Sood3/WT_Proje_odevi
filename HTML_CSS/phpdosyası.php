bee87d77bd5f419874a6ef5db4838a15aeab4c7d
Sude <167243165+Sood3@users.noreply.github.com>
2024-05-15 22:17:41 +0300

    Gün 10
    Login sayfası ve ilgi alanım(API) sayfası bitirildi son düzenlemeler ve kontroller yapıldı ve bazı karışıklıkları önlemesi için yorum blokları eklendi

--- HTML_CSS/phpdosyası.php
+++ HTML_CSS/phpdosyası.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Teknolojileri Proje Ödevi PHP sayfası</title>
+
+    <!--css link-->
     <link href="style/PHPstyle.css" rel="stylesheet" />
+
 </head>
+
 <body>
 
+<!--showing datas with php-->
 <div class="container">
         <div class="box">
             <h2><?php $name = $_POST['name']; echo "Form başarıyla gönderildi. Teşekkürler, $name!"."<br>" ?></h2>
@@ -30,9 +35,9 @@
                 <button onclick="Return()" type="button">Ana Sayfaya dön</button>
             </div>
         </div>
-        
     </div>
     
+    <!--to return main page after succesfull process with js-->
     <script>
         function Return(){
             window.location.href="AnaSayfa.html"
---
+++ HTML_CSS/phpkontroldosyasi.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+
+// makes both button hidden
+$show_success_button = false;
+$show_error_button = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    //get email without domain
+    $email_parts = explode('@', $email);
+    $email_username = $email_parts[0];
+
+    if ($email_username == $password) {
+        $_SESSION['email'] = $_POST['email'];
+        //show button if login have succeed
+        $show_success_button = true;
+    } else {
+        //show button if login have failed
+        $show_error_button = true;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP kontrol ve başarısız giriş sayfası</title>
+
+    <!--css link-->
+    <link href="style/PHPstyle.css" rel="stylesheet" />
+
+</head>
+<body>
+
+    <div class="container">
+        <div class="box">
+            <!--arranging header according to result-->
+            <h3><?php if ($show_success_button): ?>
+                Hoşgeldin, <?php echo $email_username ?>!
+            <?php endif; ?>
+
+            <?php if ($show_error_button): ?>
+                Epostanız ile şifreniz uyuşmuyor
+            <?php endif; ?></h3>
+            
+            <!--arranging buttons according to result-->
+            <div class="button-container">
+            <?php if ($show_success_button): ?>
+                <a href="AnaSayfa.html"><button type="button">Ana Sayfaya Dön</button></a>
+            <?php endif; ?>
+
+            <?php if ($show_error_button): ?>
+                <a href="Girisyap.html"><button type="button">Giriş Yap Sayfasına Dön</button></a>
+            <?php endif; ?>
+            </div>
+        </div> 
+    </div>
+
+</body>
+</html>
